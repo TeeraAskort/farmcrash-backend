9c235a129aceec13fae74d09d7d64238dd55b201
Alderaeney <alderaeney@gmail.com>
2021-11-26 09:21:06 +0100

    Fixed import

--- src/main/java/com/alderaeney/farmcrashbackend/task/TaskRepository.java
+++ src/main/java/com/alderaeney/farmcrashbackend/task/TaskRepository.java
@@ -1,5 +1,7 @@
 package com.alderaeney.farmcrashbackend.task;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
